063fa9646aae7263f37593b66aee55e79c803efb
Dikshant Koriwar <dikshantkoriwar4@gmail.com>
2026-03-04 00:44:52 +0530

    project created

---
+++ src/main/java/com/dk/care_health/CareHealthApplication.java
@@ -0,0 +1,13 @@
+package com.dk.care_health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dk/care_health/CareHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.care_health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
